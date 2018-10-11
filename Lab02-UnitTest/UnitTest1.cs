f17248e36699f9e5f2a89dbf5f2e403f1ebd4882
trecain <developedbycain@yahoo.com>
2018-10-10 23:26:11 -0500

    created xunit test and referenced lab2

---
+++ Lab02-UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab02_UnitTest;
+
+namespace Lab02_UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
