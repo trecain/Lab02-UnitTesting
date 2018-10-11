3a09dc3c91f2781debf97a8b4e3f63fe92de7525
trecain <developedbycain@yahoo.com>
2018-10-10 21:25:31 -0500

    wrote code for interface method

--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -6,7 +6,37 @@ namespace Lab02_UnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Interface();
         }
+
+        public static void Interface()
+        {
+            string userMenuSelection;
+            Console.WriteLine("1. View Balance");
+            Console.WriteLine("2. Withdraw Money");
+            Console.WriteLine("3. Add Money");
+            Console.WriteLine("4. Exit");
+            userMenuSelection = Console.ReadLine();
+
+            switch(userMenuSelection)
+            {
+                case "1":
+                    Console.WriteLine("You have selected view balance");
+                    break;
+                case "2":
+                    Console.WriteLine("You have selected to withdraw");
+                    break;
+                case "3":
+                    Console.WriteLine("You have selected to add money");
+                    break;
+                case "4":
+                    Console.WriteLine("You have selected to exit");
+                    break;
+                default:
+                    Console.WriteLine("You have selected something off the menu");
+                    break;
+            }
+        }
+
     }
 }
